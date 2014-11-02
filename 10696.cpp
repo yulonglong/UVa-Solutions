ae2613eae075bf957108534f8deb737078ba1af4
Steven Kester Yuwono <yulonglong2005@yahoo.com>
2014-11-03 02:46:41 +0800

    Solved UVa 10696

---
+++ 10696.cpp
@@ -0,0 +1,39 @@
+//Steven Kester Yuwono - UVa 10696
+//Memoization
+
+#include <cstdio>
+#include <cstring>
+#define MAX 1000100
+using namespace std;
+
+int memo[MAX];
+
+int f91(int N){
+	if(memo[N]!=-1){
+		return memo[N];
+	}
+	if(N<=100){
+		return memo[N] = f91(f91(N+11));
+	}
+	else{
+		return memo[N] = N-10;
+	}
+}
+
+int init(){
+	memset(memo,-1,sizeof(memo));
+	for(int i=0;i<=1000010;i++){
+		if(memo[i]==-1){
+			f91(i);
+		}
+	}
+}
+
+int main(){
+	init();
+	int input;
+	while(scanf("%d",&input)&&(input!=0)){
+		printf("f91(%d) = %d\n",input,memo[input]);
+	}
+	return 0;
+}
