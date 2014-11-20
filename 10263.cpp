ad9f5c24e148790db2cb8b1541455ab7fffb81de
Steven Kester Yuwono <yulonglong2005@yahoo.com>
2014-11-21 00:59:45 +0800

    Solved UVa 10263

---
+++ 10263.cpp
@@ -0,0 +1,131 @@
+//Steven Kester Yuwono - UVa 10263
+//Computational Geometry
+
+#include <algorithm>
+#include <cstdio>
+#include <cmath>
+#include <vector>
+#include <cstdio>
+#include <iostream>
+using namespace std;
+
+// BEGIN Using Geometry Library in Competitive Programming 3 book by Dr. Steven Halim
+
+#define INF 1e9
+#define EPS 1e-9
+#define PI acos(-1.0) // important constant; alternative #define PI (2.0 * acos(0.0))
+
+double DEG_to_RAD(double d) { return d * PI / 180.0; }
+
+double RAD_to_DEG(double r) { return r * 180.0 / PI; }
+
+// struct point_i { int x, y; };    // basic raw form, minimalist mode
+struct point_i { int x, y;     // whenever possible, work with point_i
+  point_i() { x = y = 0; }                      // default constructor
+  point_i(int _x, int _y) : x(_x), y(_y) {} };         // user-defined
+
+struct point { double x, y;   // only used if more precision is needed
+  point() { x = y = 0.0; }                      // default constructor
+  point(double _x, double _y) : x(_x), y(_y) {}        // user-defined
+  bool operator < (point other) const { // override less than operator
+    if (fabs(x - other.x) > EPS)                 // useful for sorting
+      return x < other.x;          // first criteria , by x-coordinate
+    return y < other.y; }          // second criteria, by y-coordinate
+  // use EPS (1e-9) when testing equality of two floating points
+  bool operator == (point other) const {
+   return (fabs(x - other.x) < EPS && (fabs(y - other.y) < EPS)); } };
+
+double dist(point p1, point p2) {                // Euclidean distance
+                      // hypot(dx, dy) returns sqrt(dx * dx + dy * dy)
+  return hypot(p1.x - p2.x, p1.y - p2.y); }           // return double
+
+struct line { double a, b, c; };          // a way to represent a line
+
+bool areParallel(line l1, line l2) {       // check coefficients a & b
+  return (fabs(l1.a-l2.a) < EPS) && (fabs(l1.b-l2.b) < EPS); }
+
+bool areSame(line l1, line l2) {           // also check coefficient c
+  return areParallel(l1 ,l2) && (fabs(l1.c - l2.c) < EPS); }
+
+// returns true (+ intersection point) if two lines are intersect
+bool areIntersect(line l1, line l2, point &p) {
+  if (areParallel(l1, l2)) return false;            // no intersection
+  // solve system of 2 linear algebraic equations with 2 unknowns
+  p.x = (l2.b * l1.c - l1.b * l2.c) / (l2.a * l1.b - l1.a * l2.b);
+  // special case: test for vertical line to avoid division by zero
+  if (fabs(l1.b) > EPS) p.y = -(l1.a * p.x + l1.c);
+  else                  p.y = -(l2.a * p.x + l2.c);
+  return true; }
+
+struct vec { double x, y;  // name: `vec' is different from STL vector
+  vec(double _x, double _y) : x(_x), y(_y) {} };
+
+vec toVec(point a, point b) {       // convert 2 points to vector a->b
+  return vec(b.x - a.x, b.y - a.y); }
+
+vec scale(vec v, double s) {        // nonnegative s = [<1 .. 1 .. >1]
+  return vec(v.x * s, v.y * s); }               // shorter.same.longer
+
+point translate(point p, vec v) {        // translate p according to v
+  return point(p.x + v.x , p.y + v.y); }
+
+double dot(vec a, vec b) { return (a.x * b.x + a.y * b.y); }
+
+double norm_sq(vec v) { return v.x * v.x + v.y * v.y; }
+
+// returns the distance from p to the line defined by
+// two points a and b (a and b must be different)
+// the closest point is stored in the 4th parameter (byref)
+double distToLine(point p, point a, point b, point &c) {
+  // formula: c = a + u * ab
+  vec ap = toVec(a, p), ab = toVec(a, b);
+  double u = dot(ap, ab) / norm_sq(ab);
+  c = translate(a, scale(ab, u));                  // translate a to c
+  return dist(p, c); }           // Euclidean distance between p and c
+
+// returns the distance from p to the line segment ab defined by
+// two points a and b (still OK if a == b)
+// the closest point is stored in the 4th parameter (byref)
+double distToLineSegment(point p, point a, point b, point &c) {
+  vec ap = toVec(a, p), ab = toVec(a, b);
+  double u = dot(ap, ab) / norm_sq(ab);
+  if (u < 0.0) { c = point(a.x, a.y);                   // closer to a
+    return dist(p, a); }         // Euclidean distance between p and a
+  if (u > 1.0) { c = point(b.x, b.y);                   // closer to b
+    return dist(p, b); }         // Euclidean distance between p and b
+  return distToLine(p, a, b, c); }          // run distToLine as above
+
+// END Library
+
+
+int main() {
+  double Xm, Ym;
+
+  while(cin >> Xm >> Ym){
+    vector<point> P;
+    point M(Xm,Ym);
+
+    int N;
+    cin >> N;
+
+    for(int i=0;i<=N;i++){
+      int x,y;
+      cin >> x >> y;
+      P.push_back(point(x,y));
+    }
+
+    double min = INF;
+    point ans;
+    for(int i=0;i<N;i++){
+      point temp;
+      double dist = distToLineSegment(M, P[i], P[i+1], temp);
+      if(dist<min){
+        min = dist;
+        ans = temp;
+      }
+    }
+    printf("%.4f\n",ans.x);
+    printf("%.4f\n",ans.y);
+  }
+  return 0;
+}
